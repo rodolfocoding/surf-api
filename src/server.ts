0b9545e5c10714689d8f363e36fe128e1ea469a6
Rodolfo Henrique Silva <rodolfo.silva@HMB.LOCAL>
2020-10-28 10:06:42 -0300

    Configurando prettier

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,10 @@ import { ForecastController } from './controllers/forecast';
 import { Application } from 'express';
 
 export class SetupServer extends Server {
-  constructor(private port = 3000){
+  constructor(private port = 3000) {
     super();
   }
-  
+
   public init(): void {
     this.setupExpress();
     this.setupControllers();
